a2e2820ce459bcdf9a891724a32061d6f8b8e959
HYDPark <DPark@linz.govt.nz>
2023-12-04 09:58:38 +1300

    Rnc panel coordinates conversion test

--- rnc.sql
+++ rnc.sql
@@ -1,51 +1,102 @@
 --Test for SRID set
-select LLDG_GEOM, sdo_geometry(pa1.LLDG_GEOM.sdo_gtype,
-       2193,
-       null,
-   pa1.LLDG_GEOM.sdo_elem_info,
-   pa1.LLDG_GEOM.sdo_ordinates) as GEOMETRY   from rnc_panel_0 pa1;
+SELECT
+   LLDG_GEOM,
+   SDO_GEOMETRY(PA1.LLDG_GEOM.SDO_GTYPE,
+   2193,
+   NULL,
+   PA1.LLDG_GEOM.SDO_ELEM_INFO,
+   PA1.LLDG_GEOM.SDO_ORDINATES) AS GEOMETRY
+FROM
+   RNC_PANEL_0 PA1;
 
 --Finding SRID
 --SRID 2193
 
-CREATE TABLE rnc_panel_v1 as (
-select chartver_id,rep_id, stringval,panelver_id,product_status,panelnumber,scale,panel, sdo_geometry(pa1.LLDG_GEOM.sdo_gtype,
-       2193,
-       null,
-   pa1.LLDG_GEOM.sdo_elem_info,
-   pa1.LLDG_GEOM.sdo_ordinates) as GEOMETRY   from rnc_panel_0 pa1);
+CREATE TABLE RNC_PANEL_V1 AS (
+   SELECT
+      CHARTVER_ID,
+      REP_ID,
+      STRINGVAL,
+      PANELVER_ID,
+      PRODUCT_STATUS,
+      PANELNUMBER,
+      SCALE,
+      PANEL,
+      SDO_GEOMETRY(PA1.LLDG_GEOM.SDO_GTYPE,
+      2193,
+      NULL,
+      PA1.LLDG_GEOM.SDO_ELEM_INFO,
+      PA1.LLDG_GEOM.SDO_ORDINATES) AS GEOMETRY
+   FROM
+      RNC_PANEL_0 PA1
+);
 
---SRID 8307  Now a couple of years ago I would said you should avoid 8307 as old-school, 
---oracle-centric and a marker of old-age. 
+--SRID 8307  Now a couple of years ago I would said you should avoid 8307 as old-school,
+--oracle-centric and a marker of old-age.
 --You should use 4326 as "everybody" knows that SRID is WGS84 geodetic 2D while only the Oracle wonks know that 8307 is.
 
-CREATE TABLE rnc_panel_v2 as (
-select chartver_id,rep_id, stringval,panelver_id,product_status,panelnumber,scale,panel, sdo_geometry(pa1.LLDG_GEOM.sdo_gtype,
-       8307,
-       null,
-   pa1.LLDG_GEOM.sdo_elem_info,
-   pa1.LLDG_GEOM.sdo_ordinates) as GEOMETRY   from rnc_panel_0 pa1);
+CREATE TABLE RNC_PANEL_V2 AS (
+   SELECT
+      CHARTVER_ID,
+      REP_ID,
+      STRINGVAL,
+      PANELVER_ID,
+      PRODUCT_STATUS,
+      PANELNUMBER,
+      SCALE,
+      PANEL,
+      SDO_GEOMETRY(PA1.LLDG_GEOM.SDO_GTYPE,
+      8307,
+      NULL,
+      PA1.LLDG_GEOM.SDO_ELEM_INFO,
+      PA1.LLDG_GEOM.SDO_ORDINATES) AS GEOMETRY
+   FROM
+      RNC_PANEL_0 PA1
+);
 
---convert googlg
-Create table rnc_panel_v3 as
-select chartver_id,rep_id, stringval,panelver_id,product_status,panelnumber,scale,panel,
-sdo_cs.transform(geometry, 8307) as geometry
-from rnc_panel_v2;
+--convert googlg coordinates
+CREATE TABLE RNC_PANEL_V3 AS
+   SELECT
+      CHARTVER_ID,
+      REP_ID,
+      STRINGVAL,
+      PANELVER_ID,
+      PRODUCT_STATUS,
+      PANELNUMBER,
+      SCALE,
+      PANEL,
+      SDO_CS.TRANSFORM(GEOMETRY, 8307) AS GEOMETRY
+   FROM
+      RNC_PANEL_V2;
 
+SELECT
+   SRID,
+   COORD_REF_SYS_NAME
+FROM
+   SDO_COORD_REF_SYS
+WHERE
+   COORD_REF_SYS_KIND = 'VERTICAL'
+ORDER BY
+   SRID;
 
+SELECT
+   *
+FROM
+   SDO_COORD_REF_SYS
+WHERE
+   SRID = 4167;
 
-SELECT srid, COORD_REF_SYS_NAME from sdo_coord_ref_sys 
-  WHERE COORD_REF_SYS_KIND = 'VERTICAL' order by srid;
-  
-  SELECT * from sdo_coord_ref_sys 
-  WHERE srid = 4167;
-
-  Create table rnc_panel_v3 as
-select chartver_id,rep_id, stringval,panelver_id,product_status,panelnumber,scale,panel,
-sdo_cs.transform(geometry, 4167) as geometry
-from rnc_panel_v2;
-
-
-
-
+CREATE TABLE RNC_PANEL_V3 AS
+   SELECT
+      CHARTVER_ID,
+      REP_ID,
+      STRINGVAL,
+      PANELVER_ID,
+      PRODUCT_STATUS,
+      PANELNUMBER,
+      SCALE,
+      PANEL,
+      SDO_CS.TRANSFORM(GEOMETRY, 4167) AS GEOMETRY
+   FROM
+      RNC_PANEL_V2;
 
