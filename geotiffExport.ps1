947ef8b2bdee5d01181bfff5942c89ee6c524550
HYDPark <DPark@linz.govt.nz>
2023-12-11 11:13:22 +1300

    Update geotiff list

--- geotiffExport.ps1
+++ geotiffExport.ps1
@@ -1,4 +1,6 @@
-﻿$File = "C:\Projects\Geotiff\LLDG.xls"  
+﻿$File = "C:\Projects\Git\Geotiff\Chart_val.xls" 
+$Save = "C:\temp\chart\"
+
 $Excel = New-Object -ComObject Excel.Application  
 $WorkBook = $Excel.Workbooks.Open($File)  
 $WorkSheet = $WorkBook.Worksheets.Item(1)  
@@ -6,9 +8,11 @@ $UsedRange = $WorkSheet.UsedRange
 $Rows = $UsedRange.Rows.Count  
  
 $Objs = @()  
-for($Row=2; $Row -le $Rows; $Row++){  
+for ($Row = 2; $Row -le $Rows; $Row++) {  
     $Objs += [PSCustomObject]@{  
-        ChartVersionId = $UsedRange.Cells($Row,1).Text    
+        ChartVersionId = $UsedRange.Cells($Row, 1).Text
+        Chartname      = $UsedRange.Cells($Row, 2).Text  
+        panelId        = $UsedRange.Cells($Row, 8).Text      
     }   
                         
 }  
@@ -18,4 +22,17 @@ $Excel.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel) | Out-Null  
 Stop-Process -Name "EXCEL"  
 
-Write-Output $Objs.ChartVersionId    
+$chartNum = $Objs.Rows.Count 
+for ($chartNum = 0; $chartNum -le $Rows - 2; $chartNum++) { 
+
+    $FilePath = ($Save + $Objs.ChartVersionId[$chartNum] + "_" + $Objs.Chartname[$chartNum] + "_" + $Objs.panelId[$chartNum] + ".tif")
+
+    If (Test-Path -path $FilePath -PathType Leaf) {
+        Write-Host "The file exists" -f Green
+        Write-Host $FilePath
+    }
+    Else {
+        carisbatch -r ExportChartToTIFF -D 300 -e EXPORT_AREA -d 8 -C "RGB(255,255,255,100)"  -g -p $Objs.panelId[$chartNum] ("hpd://lhsphpd:aE8nmHecqf2b6DzW@DEV41/db?ChartVersionId=" + $Objs.ChartVersionId[$chartNum]) $FilePath
+    }
+    
+}
